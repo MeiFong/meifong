098de7c8fd1f7f8328192dd708a94ef309262f9b
MeiFong <90292876+MeiFong@users.noreply.github.com>
2021-09-08 11:38:37 +1000

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,6 @@
+//creates an alert
+alert("Hello world!")
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function openMenu() {var x = document.getElementById ("portfolioTopnav");
+if (x.className === "topnav") {x.className += "responsive";} else {x.className = "topnav";}
+}
